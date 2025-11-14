55f941a8b5054c5b00be54201f63aeee5e24a403
Moussa <nsengimanamoussa3@gmail.com>
2025-11-14 12:39:00 +0200

    pushing videos, single video page

---
+++ src/components/vidoes/HomeVideoCard.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+export default function HomeVideoCard() {
+  return (
+    <div className="video-card-home">
+      <div className="video-thumbnail">
+        <Image src="/assets/Image.jpg" alt="Video thumbnail 1" width="300" height="169" />
+      </div>
+      <div className="card-details">
+        <Link href={`/single-video/slug`} className="text-decoration-none">
+          <span className="play-icon">
+
+            <svg width="32" height="33" viewBox="0 0 32 33" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false">
+              <g clip-path="url(#clip0_1216_2598)">
+                <path d="M16 31.43C24.4831 31.43 31.36 24.5531 31.36 16.07C31.36 7.58687 24.4831 0.709961 16 0.709961C7.51691 0.709961 0.639999 7.58687 0.639999 16.07C0.639999 24.5531 7.51691 31.43 16 31.43Z" fill="white" />
+                <path d="M16 0.0700684C7.168 0.0700684 0 7.23807 0 16.0701C0 24.9021 7.168 32.0701 16 32.0701C24.832 32.0701 32 24.9021 32 16.0701C32 7.23807 24.832 0.0700684 16 0.0700684Z" fill="#EE0120" />
+                <path d="M11.52 23.2701V8.87012L21.12 16.0701L11.52 23.2701Z" fill="white" />
+              </g>
+              <defs>
+                <clipPath id="clip0_1216_2598">
+                  <rect width="32" height="32" fill="white" transform="translate(0 0.0700684)" />
+                </clipPath>
+              </defs>
+            </svg>
+          </span>
+          <p className="heading-title">Ingabire Victoire yageze mu Rukiko…</p>
+        </Link>
+
+      </div>
+    </div>
+  )
+}
---
+++ src/hooks/useYoutubeDuration.tsx
@@ -0,0 +1,53 @@
+
+'use client';
+import { useEffect, useState } from "react";
+
+function parseYoutubeDuration(iso: string) {
+  const match = iso.match(/PT(?:(\d+)H)?(?:(\d+)M)?(?:(\d+)S)?/);
+  if (!match) return "0:00";
+
+  const hours = parseInt(match[1] || "0");
+  const mins = parseInt(match[2] || "0");
+  const secs = parseInt(match[3] || "0");
+
+  const total = hours * 3600 + mins * 60 + secs;
+  const h = Math.floor(total / 3600);
+  const m = Math.floor((total % 3600) / 60);
+  const s = total % 60;
+
+//   return h > 0 ? `${h}:${m.toString().padStart(2, "0")}:${s.toString().padStart(2, "0")}` : `${m}:${s.toString().padStart(2, "0")}`;
+  return h > 0 ? `${h}:${m.toString().padStart(2, "0")}` : `${m}`;
+}
+
+
+export function useYoutubeDuration(videoId: string | null) {
+
+  const [duration, setDuration] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (!videoId) return;
+
+    async function fetchDuration() {
+      try {
+        // const apiKey = process.env.NEXT_PUBLIC_YOUTUBE_API_KEY;
+        const apiKey = "AIzaSyAoGM17iUisP-ia2F0jZC_0TElRQOmNJEo";
+     
+        const url = `https://www.googleapis.com/youtube/v3/videos?id=${videoId}&part=contentDetails&key=${apiKey}`;
+
+        const res = await fetch(url);
+
+        const data = await res.json();
+        
+
+        const iso = data?.items?.[0]?.contentDetails?.duration;
+        if (iso) setDuration(parseYoutubeDuration(iso));
+      } catch (err) {
+        console.error("YT API error:", err);
+      }
+    }
+
+    fetchDuration();
+  }, [videoId]);
+
+  return duration;
+}
